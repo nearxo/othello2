64ddc4f09d5a83b29cfc3c6b8f7221730bc45fd8
nearxo <66927680+nearxo@users.noreply.github.com>
2021-04-13 16:56:35 -0500

    Update and rename Eseste.java to Main.java

--- othello2/Main.java
+++ othello2/Main.java
@@ -2,7 +2,7 @@ package othello2;
 
 import java.awt.EventQueue;
 
-public class Eseste {
+public class Main {
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
